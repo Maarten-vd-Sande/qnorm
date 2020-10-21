64f07b6101647d9bfa39dae2994f31fff8729aaf
Maarten-vd-Sande <maartenvandersande@hotmail.com>
2020-10-21 13:52:50 +0200

    v0.6.1

--- qnorm/__init__.py
+++ qnorm/__init__.py
@@ -4,4 +4,4 @@ from .quantile_normalize import quantile_normalize, quantile_normalize_file
 __all__ = ["quantile_normalize", "quantile_normalize_file"]
 __author__ = "Maarten van der Sande"
 __email__ = "maartenvandersande[at]gmail.com"
-__version__ = "0.6.0"
+__version__ = "0.6.1"
