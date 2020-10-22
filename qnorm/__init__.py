083ca400da7da35d31913b3bf55714bfd2bad82e
Maarten-vd-Sande <maartenvandersande@hotmail.com>
2020-10-22 10:22:13 +0200

    fix imports

--- qnorm/__init__.py
+++ qnorm/__init__.py
@@ -1,7 +1,13 @@
 """Top-level package for qnorm."""
-from .quantile_normalize import quantile_normalize, quantile_normalize_file
+from .quantile_normalize import quantile_normalize
+__all__ = ["quantile_normalize"]
 
-__all__ = ["quantile_normalize", "quantile_normalize_file"]
+try:
+    from .quantile_normalize import quantile_normalize_file
+    __all__.append("quantile_normalize_file")
+except:
+    pass
+
 __author__ = "Maarten van der Sande"
 __email__ = "maartenvandersande[at]gmail.com"
 __version__ = "0.6.1"
